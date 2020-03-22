7b276568c6715d6aec68a2422693fca44e8e4971
Jonathan <jonathanyen91034@gmail.com>
2020-03-22 11:46:58 +0800

    second version done

--- password.py
+++ password.py
@@ -1,13 +1,18 @@
 password = '1004'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('please enter your password: ')	
 	if pwd == password:
 	    print('login successfully') 
 	    break
 	else:
-		i = i - 1
-		print('wrong password' , i, 'more chance' )
+		
+		print('wrong password' )
+		if i > 0:
+			print( i, 'more chance')
+		else:
+			print('delete all data')
 		
 			
 
