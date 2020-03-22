8a62b032bb06e8130949d537357469e65befece4
Jonathan <jonathanyen91034@gmail.com>
2020-03-22 11:32:05 +0800

    change while i > 0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 password = '1004'
 i = 3
-while True:
+while i > 0:
 	pwd = input('please enter your password: ')	
 	if pwd == password:
 	    print('login successfully') 
@@ -8,6 +8,6 @@ while True:
 	else:
 		i = i - 1
 		print('wrong password' , i, 'more chance' )
-		if i == 0:
-			break
+		
+			
 
